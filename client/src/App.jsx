23c9d66b0ed421bf1ac06fac8402b158c0aefe66
Sajjat Hossain <Sajjad Sadiq>
2023-01-17 01:45:24 +0600

    react router dom install & project router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import UserName from "./components/UserName";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserName />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/page-not-found",
+    element: <PageNotFound />,
+  },
+]);
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/routes/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Root Router</div>
+    }
+])
+
+export default router
